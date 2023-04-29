08a1957b153f64c123154a1cd92f5e30c567d43d
Ovy Evbodi <evbodiovo@gmail.com>
2023-04-29 19:10:21 +0100

    Feat: Add 404 error json response

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 """Renders api data using Flask"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -11,17 +11,23 @@ def create_app():
     app.register_blueprint(app_views)
     return app
 
-    @app.teardown_appcontext
-    def shutdown(self):
-        """Tear down function for app context"""
-
-        storage.close()
-
 
 if __name__ == '__main__':
     app = create_app()
     port_env = getenv('HBNB_API_PORT', '5000')
     host_env = getenv('HBNB_API_HOST', '0.0.0.0')
     app.config['PRETTYPRINT'] = True
+
+    @app.teardown_appcontext
+    def shutdown(self):
+        """Tear down function for app context"""
+
+        storage.close()
+
+    @app.errorhandler(404)
+    def not_found(error):
+        """Error response for the 404 status code"""
+
+        return make_response(jsonify({"error": "Not found"}), 404)
 
     app.run(host=host_env, port=port_env, threaded=True)
